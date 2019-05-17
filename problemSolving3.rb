df5bc30c07b37972f82d7093217e742dd17d10f5
syauqiahmd <ahmadsyauqi@merahputih.id>
2019-05-17 16:39:40 +0800

    top 3 for q 3

--- problemSolving3.rb
+++ problemSolving3.rb
@@ -1,7 +1,14 @@
 def most_text(file_path)
+  mostText = []
   file = File.read(file_path).delete("\n").gsub(/[,.?!]/, "")
-  splitText = file.split.group_by(&:downcase).map {|key,value| [key, value.length]}
-  return splitText.max_by {|key, val| val}
+  splitText = file.split.group_by(&:downcase).map {|key,value| [key, value.length]}.sort_by{|key, value| -value}
+  splitText.each do |index|
+    mostText.push(index)
+    if mostText.length == 3
+      break
+    end
+  end
+  return mostText
 end
 
 puts most_text(ARGV[0])
